1d8449f79242b5b9946f14ab967914eccec228ab
goviel <leivajose@gmail.com>
2016-11-27 13:01:06 -0600

    Changed a few things on the Header for now

--- CardGame/src/matchGame/MatchGame.java
+++ CardGame/src/matchGame/MatchGame.java
@@ -1,15 +1,16 @@
-      /**Class: Programming Fundamentals
+      /**Class: Programming Fundamentals III
 
-        *Project #  : Group Project #1
+        *Project #  : Group Project
 
-        *Students:    Hector Solis, Jackson Martinez, Jessie Billingsly
+        *Students:    Jackson Martinez
 
-        *Completion Date: 3/26/2014
+        *Completion Date: 
 
         *Project Description: This program is a matching game, the user will be presented with a target icon and an array of buttons. The user will have to match the target icon with buttons with the same icons from the button array within
         *a specified time. The user will earn points 100 points for every correctly matched icon and lose 100 points for every miss-matched icons. If the button clicked is a match with the target, the button will highlight green. If the button clicked
         *is NOT a match with the target, the button will highlight red. After the game expires, the user may end the game or start a new game.
        **/
+
 
 package matchGame;
 import java.awt.event.*;
